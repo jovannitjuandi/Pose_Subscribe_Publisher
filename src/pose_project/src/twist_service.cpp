ec574001cdc72457aee0fc6c366cd14aa65153df
jovannitjuandi <jovanni.tjuandi@gmail.com>
2021-07-08 21:07:56 +1000

    Add Proper Time Difference

--- src/pose_project/src/twist_service.cpp
+++ src/pose_project/src/twist_service.cpp
@@ -3,19 +3,21 @@
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Twist.h"
 
-// TODO: Setup Time Calculation
+// TODO: SETUP ORIENTATION FROM DEGREES TO QUATERNIONS
 
 geometry_msgs::Twist twist;
 geometry_msgs::PoseStamped previousPose;
 
 void twistConverter (const geometry_msgs::PoseStamped::ConstPtr&msg) {
-    twist.linear.x = (previousPose.pose.orientation.x - msg->pose.position.x)/();
-    twist.linear.y = msg->pose.position.y;
-    twist.linear.z = msg->pose.position.z;
+    auto timeDifference = (msg->header.stamp.sec + msg->header.stamp.sec/(1000000000.0)) - (previousPose.header.stamp.sec + previousPose.header.stamp.nsec/(1000000000.0));
 
-    twist.angular.x = msg->pose.orientation.x;
-    twist.angular.y = msg->pose.orientation.y;
-    twist.angular.z = msg->pose.orientation.z;
+    twist.linear.x = (msg->pose.position.x - previousPose.pose.position.x)/(timeDifference);
+    twist.linear.y = (msg->pose.position.y - previousPose.pose.position.y)/(timeDifference);
+    twist.linear.z = (msg->pose.position.z - previousPose.pose.position.z)/(timeDifference);
+
+    twist.angular.x = (msg->pose.orientation.x - previousPose.pose.position.x)/(timeDifference);
+    twist.angular.y = (msg->pose.orientation.y - previousPose.pose.position.y)/(timeDifference);
+    twist.angular.z = (msg->pose.orientation.z - previousPose.pose.position.z)/(timeDifference);
 
     previousPose.pose.position.x = msg->pose.position.x;
     previousPose.pose.position.y = msg->pose.position.y;
@@ -25,8 +27,8 @@ void twistConverter (const geometry_msgs::PoseStamped::ConstPtr&msg) {
     previousPose.pose.orientation.y = msg->pose.orientation.y;
     previousPose.pose.orientation.z = msg->pose.orientation.z;
 
-    previousPose.header.time.sec = msg->header.time.sec; 
-    previousPose.header.time.nsec = msg->header.time.nsec; 
+    previousPose.header.stamp.sec = msg->header.stamp.sec; 
+    previousPose.header.stamp.nsec = msg->header.stamp.nsec; 
 }
 
 int main (int argc, char** argv) {
@@ -36,7 +38,7 @@ int main (int argc, char** argv) {
 	ros::NodeHandle n;
 	ros::Subscriber posesub = n.subscribe("posetopic", 1000, twistConverter);
 	ros::Publisher twistpub = n.advertise<geometry_msgs::Twist>("twisttopic", 200);
-	ros::Rate loop_rate(5);
+	ros::Rate loop_rate(10);
 
     twist.linear.x = 0;
     twist.linear.y = 0;
@@ -52,7 +54,6 @@ int main (int argc, char** argv) {
 	    ros::spinOnce();
 	    loop_rate.sleep();
     }
-
 
 	return 0;
 }
